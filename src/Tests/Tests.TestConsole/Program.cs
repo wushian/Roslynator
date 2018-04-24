4dda410aff28c5253ff1659aadab9e1fbc32cea5
Josef Pihrt <josef.pihrt@outlook.com>
2018-04-24 18:28:58 +0200

    x

--- src/Tests/Tests.TestConsole/Program.cs
+++ src/Tests/Tests.TestConsole/Program.cs
@@ -1,12 +1,19 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+#region usings
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
+using Roslynator;
+using Roslynator.CSharp;
+using Roslynator.CSharp.Syntax;
+#endregion usings
 
 namespace Roslynator.Tests
 {
@@ -15,6 +22,10 @@ namespace Roslynator.Tests
         internal static async Task Main()
         {
             const string source = @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 class C
 {
     void M()
@@ -30,7 +41,17 @@ class C
                 .AddProject(projectId, "TestProject", "TestProject", LanguageNames.CSharp)
                 .AddMetadataReferences(
                     projectId,
-                    new MetadataReference[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location), })
+                    new MetadataReference[]
+                    {
+                        MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                        RuntimeMetadataReference.CreateFromAssemblyName("System.Core.dll"),
+                        RuntimeMetadataReference.CreateFromAssemblyName("System.Linq.dll"),
+                        RuntimeMetadataReference.CreateFromAssemblyName("System.Linq.Expressions.dll"),
+                        RuntimeMetadataReference.CreateFromAssemblyName("System.Runtime.dll"),
+                        RuntimeMetadataReference.CreateFromAssemblyName("System.Collections.Immutable.dll"),
+                        RuntimeMetadataReference.CreateFromAssemblyName("Microsoft.CodeAnalysis.dll"),
+                        RuntimeMetadataReference.CreateFromAssemblyName("Microsoft.CodeAnalysis.CSharp.dll"),
+                    })
                 .GetProject(projectId);
 
             var parseOptions = (CSharpParseOptions)project.ParseOptions;
