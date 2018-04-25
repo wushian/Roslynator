f6c1833e9b8e19147be4db65b33e5f4a812c8e0e
Josef Pihrt <josef.pihrt@outlook.com>
2018-04-25 19:38:53 +0200

    x

--- src/Tests/Tests.Utilities/FileUtility.cs
+++ src/Tests/Tests.Utilities/FileUtility.cs
@@ -8,23 +8,24 @@ namespace Roslynator
     {
         public const string TestFileName = "Test";
         public const string TestProjectName = "TestProject";
-
         public const string CSharpFileExtension = "cs";
         public const string VisualBasicFileExtension = "vb";
-
-        internal const int FileNumberingBase = 0;
-
         public const string DefaultCSharpFileName = TestFileName + "0." + CSharpFileExtension;
         public const string DefaultVisualBasicFileName = TestFileName + "0." + VisualBasicFileExtension;
+        internal const int FileNumberingBase = 0;
 
         public static string CreateDefaultFileName(string language)
         {
-            return (language == LanguageNames.CSharp) ? DefaultCSharpFileName : DefaultVisualBasicFileName;
+            return (language == LanguageNames.CSharp)
+                ? DefaultCSharpFileName
+                : DefaultVisualBasicFileName;
         }
 
         public static string CreateFileName(string fileName = TestFileName, int suffix = FileNumberingBase, string language = LanguageNames.CSharp)
         {
-            string extension = ((language == LanguageNames.CSharp) ? CSharpFileExtension : VisualBasicFileExtension);
+            string extension = ((language == LanguageNames.CSharp)
+                ? CSharpFileExtension
+                : VisualBasicFileExtension);
 
             return $"{fileName}{suffix}.{extension}";
         }
