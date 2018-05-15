ff62269f444705c67e3581dbdf827ba52fd8b1dc
Josef Pihrt <josef.pihrt@outlook.com>
2018-05-15 16:01:00 +0200

    x

---
+++ tools/test.cmd
@@ -0,0 +1,36 @@
+@echo off
+
+"C:\Program Files\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild" "..\src\Roslynator.sln" ^
+ /t:Build ^
+ /p:Configuration=DebugTests,RunCodeAnalysis=false ^
+ /v:minimal
+
+if errorlevel 1 (
+ pause
+ exit
+)
+
+
+dotnet test -c Debug --no-build "..\src\Tests\Analyzers.Tests\Analyzers.Tests.csproj"
+
+if errorlevel 1 (
+ pause
+ exit
+)
+
+dotnet test -c Debug --no-build "..\src\Tests\CodeFixes.Tests\CodeFixes.Tests.csproj"
+
+if errorlevel 1 (
+ pause
+ exit
+)
+
+dotnet test -c Debug --no-build "..\src\Tests\Refactorings.Tests\Refactorings.Tests.csproj"
+
+if errorlevel 1 (
+ pause
+ exit
+)
+
+echo OK
+pause
