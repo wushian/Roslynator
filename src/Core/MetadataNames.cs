e8700ff7d80e5cd50305e1b2eec86d6ebbee0e19
Josef Pihrt <josef.pihrt@outlook.com>
2018-11-21 16:53:55 +0100

    Use method MetadataName.Parse

--- src/Core/MetadataNames.cs
+++ src/Core/MetadataNames.cs
@@ -1,73 +1,51 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Collections.Immutable;
-
 namespace Roslynator
 {
     internal static class MetadataNames
     {
-        public static readonly MetadataName System_ArgumentException = new MetadataName(Namespaces.System, "ArgumentException");
-        public static readonly MetadataName System_ArgumentNullException = new MetadataName(Namespaces.System, "ArgumentNullException");
-        public static readonly MetadataName System_AttributeUsageAttribute = new MetadataName(Namespaces.System, "AttributeUsageAttribute");
-        public static readonly MetadataName System_Collections_Generic = new MetadataName(Namespaces.System_Collections, "Generic");
-        public static readonly MetadataName System_Collections_Generic_List_T = new MetadataName(Namespaces.System_Collections_Generic, "List`1");
-        public static readonly MetadataName System_Collections_IDictionary = new MetadataName(Namespaces.System_Collections, "IDictionary");
-        public static readonly MetadataName System_Collections_Immutable_ImmutableArray_T = new MetadataName(Namespaces.System_Collections_Immutable, "ImmutableArray`1");
-        public static readonly MetadataName System_ComponentModel_INotifyPropertyChanged = new MetadataName(Namespaces.System_ComponentModel, "INotifyPropertyChanged");
-        public static readonly MetadataName System_Diagnostics = new MetadataName(Namespaces.System, "Diagnostics");
-        public static readonly MetadataName System_Diagnostics_CodeAnalysis = new MetadataName(Namespaces.System_Diagnostics, "CodeAnalysis");
-        public static readonly MetadataName System_Diagnostics_Debug = new MetadataName(Namespaces.System_Diagnostics, "Debug");
-        public static readonly MetadataName System_Diagnostics_DebuggerDisplayAttribute = new MetadataName(Namespaces.System_Diagnostics, "DebuggerDisplayAttribute");
-        public static readonly MetadataName System_Enum = new MetadataName(Namespaces.System, "Enum");
-        public static readonly MetadataName System_EventArgs = new MetadataName(Namespaces.System, "EventArgs");
-        public static readonly MetadataName System_EventHandler = new MetadataName(Namespaces.System, "EventHandler");
-        public static readonly MetadataName System_FlagsAttribute = new MetadataName(Namespaces.System, "FlagsAttribute");
-        public static readonly MetadataName System_Func_T2 = new MetadataName(Namespaces.System, "Func`2");
-        public static readonly MetadataName System_Func_T3 = new MetadataName(Namespaces.System, "Func`3");
-        public static readonly MetadataName System_IEquatable_T = new MetadataName(Namespaces.System, "IEquatable`1");
-        public static readonly MetadataName System_Linq_Enumerable = new MetadataName(Namespaces.System_Linq, "Enumerable");
-        public static readonly MetadataName System_Linq_Expressions_Expression_T = new MetadataName(Namespaces.System_Linq_Expressions, "Expression`1");
-        public static readonly MetadataName System_Linq_ImmutableArrayExtensions = new MetadataName(Namespaces.System_Linq, "ImmutableArrayExtensions");
-        public static readonly MetadataName System_Linq_IOrderedEnumerable_T = new MetadataName(Namespaces.System_Linq, "IOrderedEnumerable`1");
-        public static readonly MetadataName System_Linq_IQueryable_T = new MetadataName(Namespaces.System_Linq, "IQueryable`1");
-        public static readonly MetadataName System_NonSerializedAttribute = new MetadataName(Namespaces.System, "NonSerializedAttribute");
-        public static readonly MetadataName System_ObsoleteAttribute = new MetadataName(Namespaces.System, "ObsoleteAttribute");
-        public static readonly MetadataName System_Reflection = new MetadataName(Namespaces.System, "Reflection");
-        public static readonly MetadataName System_Runtime_CompilerServices = new MetadataName(Namespaces.System_Runtime, "CompilerServices");
-        public static readonly MetadataName System_Runtime_CompilerServices_ConfiguredTaskAwaitable = new MetadataName(Namespaces.System_Runtime_CompilerServices, "ConfiguredTaskAwaitable");
-        public static readonly MetadataName System_Runtime_CompilerServices_ConfiguredTaskAwaitable_T = new MetadataName(Namespaces.System_Runtime_CompilerServices, "ConfiguredTaskAwaitable`1");
-        public static readonly MetadataName System_Runtime_InteropServices_LayoutKind = new MetadataName(Namespaces.System_Runtime_InteropServices, "LayoutKind");
-        public static readonly MetadataName System_Runtime_InteropServices_StructLayoutAttribute = new MetadataName(Namespaces.System_Runtime_InteropServices, "StructLayoutAttribute");
-        public static readonly MetadataName System_Runtime_Serialization_DataMemberAttribute = new MetadataName(Namespaces.System_Runtime_Serialization, "DataMemberAttribute");
-        public static readonly MetadataName System_Runtime_Serialization_SerializationInfo = new MetadataName(Namespaces.System_Runtime_Serialization, "SerializationInfo");
-        public static readonly MetadataName System_Runtime_Serialization_StreamingContext = new MetadataName(Namespaces.System_Runtime_Serialization, "StreamingContext");
-        public static readonly MetadataName System_Text_RegularExpressions_Regex = new MetadataName(Namespaces.System_Text_RegularExpressions, "Regex");
-        public static readonly MetadataName System_Text_RegularExpressions_RegexOptions = new MetadataName(Namespaces.System_Text_RegularExpressions, "RegexOptions");
-        public static readonly MetadataName System_Text_StringBuilder = new MetadataName(Namespaces.System_Text, "StringBuilder");
-        public static readonly MetadataName System_Threading_Tasks = new MetadataName(Namespaces.System_Threading, "Tasks");
-        public static readonly MetadataName System_Threading_Tasks_Task = new MetadataName(Namespaces.System_Threading_Tasks, "Task");
-        public static readonly MetadataName System_Threading_Tasks_Task_T = new MetadataName(Namespaces.System_Threading_Tasks, "Task`1");
-        public static readonly MetadataName System_Threading_Tasks_ValueTask_T = new MetadataName(Namespaces.System_Threading_Tasks, "ValueTask`1");
-        public static readonly MetadataName System_TimeSpan = new MetadataName(Namespaces.System, "TimeSpan");
-
-        private static class Namespaces
-        {
-            public static readonly ImmutableArray<string> System = ImmutableArray.Create("System");
-            public static readonly ImmutableArray<string> System_Collections = ImmutableArray.Create("System", "Collections");
-            public static readonly ImmutableArray<string> System_Collections_Generic = ImmutableArray.Create("System", "Collections", "Generic");
-            public static readonly ImmutableArray<string> System_Collections_Immutable = ImmutableArray.Create("System", "Collections", "Immutable");
-            public static readonly ImmutableArray<string> System_ComponentModel = ImmutableArray.Create("System", "ComponentModel");
-            public static readonly ImmutableArray<string> System_Diagnostics = ImmutableArray.Create("System", "Diagnostics");
-            public static readonly ImmutableArray<string> System_Linq = ImmutableArray.Create("System", "Linq");
-            public static readonly ImmutableArray<string> System_Linq_Expressions = ImmutableArray.Create("System", "Linq", "Expressions");
-            public static readonly ImmutableArray<string> System_Runtime = ImmutableArray.Create("System", "Runtime");
-            public static readonly ImmutableArray<string> System_Runtime_CompilerServices = ImmutableArray.Create("System", "Runtime", "CompilerServices");
-            public static readonly ImmutableArray<string> System_Runtime_InteropServices = ImmutableArray.Create("System", "Runtime", "InteropServices");
-            public static readonly ImmutableArray<string> System_Runtime_Serialization = ImmutableArray.Create("System", "Runtime", "Serialization");
-            public static readonly ImmutableArray<string> System_Text = ImmutableArray.Create("System", "Text");
-            public static readonly ImmutableArray<string> System_Text_RegularExpressions = ImmutableArray.Create("System", "Text", "RegularExpressions");
-            public static readonly ImmutableArray<string> System_Threading = ImmutableArray.Create("System", "Threading");
-            public static readonly ImmutableArray<string> System_Threading_Tasks = ImmutableArray.Create("System", "Threading", "Tasks");
-        }
+        public static readonly MetadataName System_ArgumentException = MetadataName.Parse("System.ArgumentException");
+        public static readonly MetadataName System_ArgumentNullException = MetadataName.Parse("System.ArgumentNullException");
+        public static readonly MetadataName System_AttributeUsageAttribute = MetadataName.Parse("System.AttributeUsageAttribute");
+        public static readonly MetadataName System_Collections_Generic = MetadataName.Parse("System.Collections.Generic");
+        public static readonly MetadataName System_Collections_Generic_List_T = MetadataName.Parse("System.Collections.Generic.List`1");
+        public static readonly MetadataName System_Collections_IDictionary = MetadataName.Parse("System.Collections.IDictionary");
+        public static readonly MetadataName System_Collections_Immutable_ImmutableArray_T = MetadataName.Parse("System.Collections.Immutable.ImmutableArray`1");
+        public static readonly MetadataName System_ComponentModel_INotifyPropertyChanged = MetadataName.Parse("System.ComponentModel.INotifyPropertyChanged");
+        public static readonly MetadataName System_Diagnostics = MetadataName.Parse("System.Diagnostics");
+        public static readonly MetadataName System_Diagnostics_CodeAnalysis = MetadataName.Parse("System.Diagnostics.CodeAnalysis");
+        public static readonly MetadataName System_Diagnostics_Debug = MetadataName.Parse("System.Diagnostics.Debug");
+        public static readonly MetadataName System_Diagnostics_DebuggerDisplayAttribute = MetadataName.Parse("System.Diagnostics.DebuggerDisplayAttribute");
+        public static readonly MetadataName System_Enum = MetadataName.Parse("System.Enum");
+        public static readonly MetadataName System_EventArgs = MetadataName.Parse("System.EventArgs");
+        public static readonly MetadataName System_EventHandler = MetadataName.Parse("System.EventHandler");
+        public static readonly MetadataName System_FlagsAttribute = MetadataName.Parse("System.FlagsAttribute");
+        public static readonly MetadataName System_Func_T2 = MetadataName.Parse("System.Func`2");
+        public static readonly MetadataName System_Func_T3 = MetadataName.Parse("System.Func`3");
+        public static readonly MetadataName System_IEquatable_T = MetadataName.Parse("System.IEquatable`1");
+        public static readonly MetadataName System_Linq_Enumerable = MetadataName.Parse("System.Linq.Enumerable");
+        public static readonly MetadataName System_Linq_Expressions_Expression_T = MetadataName.Parse("System.Linq.Expressions.Expression`1");
+        public static readonly MetadataName System_Linq_ImmutableArrayExtensions = MetadataName.Parse("System.Linq.ImmutableArrayExtensions");
+        public static readonly MetadataName System_Linq_IOrderedEnumerable_T = MetadataName.Parse("System.Linq.IOrderedEnumerable`1");
+        public static readonly MetadataName System_Linq_IQueryable_T = MetadataName.Parse("System.Linq.IQueryable`1");
+        public static readonly MetadataName System_NonSerializedAttribute = MetadataName.Parse("System.NonSerializedAttribute");
+        public static readonly MetadataName System_ObsoleteAttribute = MetadataName.Parse("System.ObsoleteAttribute");
+        public static readonly MetadataName System_Reflection = MetadataName.Parse("System.Reflection");
+        public static readonly MetadataName System_Runtime_CompilerServices = MetadataName.Parse("System.Runtime.CompilerServices");
+        public static readonly MetadataName System_Runtime_CompilerServices_ConfiguredTaskAwaitable = MetadataName.Parse("System.Runtime.CompilerServices.ConfiguredTaskAwaitable");
+        public static readonly MetadataName System_Runtime_CompilerServices_ConfiguredTaskAwaitable_T = MetadataName.Parse("System.Runtime.CompilerServices.ConfiguredTaskAwaitable`1");
+        public static readonly MetadataName System_Runtime_InteropServices_LayoutKind = MetadataName.Parse("System.Runtime.InteropServices.LayoutKind");
+        public static readonly MetadataName System_Runtime_InteropServices_StructLayoutAttribute = MetadataName.Parse("System.Runtime.InteropServices.StructLayoutAttribute");
+        public static readonly MetadataName System_Runtime_Serialization_DataMemberAttribute = MetadataName.Parse("System.Runtime.Serialization.DataMemberAttribute");
+        public static readonly MetadataName System_Runtime_Serialization_SerializationInfo = MetadataName.Parse("System.Runtime.Serialization.SerializationInfo");
+        public static readonly MetadataName System_Runtime_Serialization_StreamingContext = MetadataName.Parse("System.Runtime.Serialization.StreamingContext");
+        public static readonly MetadataName System_Text_RegularExpressions_Regex = MetadataName.Parse("System.Text.RegularExpressions.Regex");
+        public static readonly MetadataName System_Text_RegularExpressions_RegexOptions = MetadataName.Parse("System.Text.RegularExpressions.RegexOptions");
+        public static readonly MetadataName System_Text_StringBuilder = MetadataName.Parse("System.Text.StringBuilder");
+        public static readonly MetadataName System_Threading_Tasks = MetadataName.Parse("System.Threading.Tasks");
+        public static readonly MetadataName System_Threading_Tasks_Task = MetadataName.Parse("System.Threading.Tasks.Task");
+        public static readonly MetadataName System_Threading_Tasks_Task_T = MetadataName.Parse("System.Threading.Tasks.Task`1");
+        public static readonly MetadataName System_Threading_Tasks_ValueTask_T = MetadataName.Parse("System.Threading.Tasks.ValueTask`1");
+        public static readonly MetadataName System_TimeSpan = MetadataName.Parse("System.TimeSpan");
     }
 }
