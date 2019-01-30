ba03fe2d96be6f801a19fdf03ac0c525adcb7ccd
Josef Pihrt <josef.pihrt@outlook.com>
2019-01-30 02:49:20 +0100

    x

--- tools/_analyze-unused.cmd
+++ tools/_analyze-unused.cmd
@@ -1,8 +1,11 @@
 @echo off
 
-"C:\Program Files\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild" "..\src\CommandLine.sln" /t:Build /p:Configuration=Debug /v:m /m
+set _msbuildPath="C:\Program Files\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin"
+
+%_msbuildPath%\msbuild "..\src\CommandLine.sln" /t:Build /p:Configuration=Debug /v:m /m
 
 "..\src\CommandLine\bin\Debug\net461\roslynator" analyze-unused "..\src\Roslynator.sln" ^
+ --msbuild-path %_msbuildPath% ^
  --visibility internal ^
  --scope type ^
  --verbosity d ^
