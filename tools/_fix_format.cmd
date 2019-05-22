e438c49c830456312e4a3ef40fc70105eac3bc98
Josef Pihrt <josef.pihrt@outlook.com>
2019-05-22 12:47:22 +0200

    x

---
+++ tools/_fix_format.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+set _msbuildPath="C:\Program Files\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin"
+
+%_msbuildPath%\msbuild "..\src\CommandLine.sln" /t:Build /p:Configuration=Debug /v:m /m
+
+"..\src\CommandLine\bin\Debug\net461\roslynator" fix "..\src\Roslynator.sln" ^
+ --msbuild-path %_msbuildPath% ^
+ --analyzer-assemblies ^
+  "..\src\Formatting.Analyzers.CodeFixes\bin\Debug\netstandard1.3\Roslynator.Formatting.Analyzers.dll" ^
+  "..\src\Formatting.Analyzers.CodeFixes\bin\Debug\netstandard1.3\Roslynator.Formatting.Analyzers.CodeFixes.dll" ^
+ --format ^
+ --verbosity d ^
+ --file-log "roslynator.log" ^
+ --file-log-verbosity diag ^
+ --diagnostic-fix-map "RCS1155=Roslynator.RCS1155.OrdinalIgnoreCase" ^
+ --file-banner " Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information."
+
+pause
