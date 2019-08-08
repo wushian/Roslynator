380e2329ba1a805a47f8a4d95f79972452457dfa
Josef Pihrt <josef.pihrt@outlook.com>
2019-08-09 01:22:19 +0200

    x

--- tools/vscode.package.cmd
+++ tools/vscode.package.cmd
@@ -4,16 +4,16 @@ cd ..\src\VisualStudioCode\package
 
 del /S /Q roslyn
 
-copy /Y ..\..\VisualStudio\bin\Release\Roslynator.Core.dll* roslyn\common
-copy /Y ..\..\VisualStudio\bin\Release\Roslynator.Common.dll* roslyn\common
-copy /Y ..\..\VisualStudio\bin\Release\Roslynator.CSharp.dll* roslyn\common
-copy /Y ..\..\VisualStudio\bin\Release\Roslynator.Workspaces.Core.dll* roslyn\common
-copy /Y ..\..\VisualStudio\bin\Release\Roslynator.Workspaces.Common.dll* roslyn\common
-copy /Y ..\..\VisualStudio\bin\Release\Roslynator.CSharp.Workspaces.dll* roslyn\common
-copy /Y ..\..\VisualStudio\bin\Release\Roslynator.CSharp.Analyzers.dll* roslyn\analyzers
-copy /Y ..\..\VisualStudio\bin\Release\Roslynator.CSharp.Analyzers.CodeFixes.dll* roslyn\analyzers
-copy /Y ..\..\VisualStudio\bin\Release\Roslynator.CSharp.Refactorings.dll* roslyn\refactorings
-copy /Y ..\..\VisualStudio\bin\Release\Roslynator.CSharp.CodeFixes.dll* roslyn\fixes
+copy /Y ..\..\VisualStudio\bin\Release\Roslynator.Core.dll roslyn\common
+copy /Y ..\..\VisualStudio\bin\Release\Roslynator.Common.dll roslyn\common
+copy /Y ..\..\VisualStudio\bin\Release\Roslynator.CSharp.dll roslyn\common
+copy /Y ..\..\VisualStudio\bin\Release\Roslynator.Workspaces.Core.dll roslyn\common
+copy /Y ..\..\VisualStudio\bin\Release\Roslynator.Workspaces.Common.dll roslyn\common
+copy /Y ..\..\VisualStudio\bin\Release\Roslynator.CSharp.Workspaces.dll roslyn\common
+copy /Y ..\..\VisualStudio\bin\Release\Roslynator.CSharp.Analyzers.dll roslyn\analyzers
+copy /Y ..\..\VisualStudio\bin\Release\Roslynator.CSharp.Analyzers.CodeFixes.dll roslyn\analyzers
+copy /Y ..\..\VisualStudio\bin\Release\Roslynator.CSharp.Refactorings.dll roslyn\refactorings
+copy /Y ..\..\VisualStudio\bin\Release\Roslynator.CSharp.CodeFixes.dll roslyn\fixes
 
 vsce package
 
