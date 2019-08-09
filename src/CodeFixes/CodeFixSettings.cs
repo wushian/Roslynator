415abfa1e36f8c507ab33b6fa150811212e8ae95
Josef Pihrt <josef.pihrt@outlook.com>
2019-08-09 20:41:45 +0200

    x

---
+++ src/CodeFixes/CodeFixSettings.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Roslynator
+{
+    public sealed class CodeFixSettings : CodeAnalysisSettings<CodeFixIdentifier>
+    {
+        public static CodeFixSettings Current { get; } = Load();
+
+        private static CodeFixSettings Load()
+        {
+            var settings = new CodeFixSettings();
+#if VSCODE
+            foreach (KeyValuePair<string, bool> kvp in VisualStudioCode.Configuration.CodeFixes)
+            {
+                string id = kvp.Key;
+                bool isEnabled = kvp.Value;
+
+                if (CodeFixIdentifier.TryParse(id, out CodeFixIdentifier codeFixIdentifier))
+                {
+                    settings.Set(codeFixIdentifier, isEnabled);
+                }
+                else if (id.StartsWith(CodeFixIdentifier.CodeFixIdPrefix, StringComparison.Ordinal))
+                {
+                    foreach (string compilerDiagnosticId in CodeFixMap.GetCompilerDiagnosticIds(id))
+                    {
+                        settings.Set(compilerDiagnosticId, id, isEnabled);
+                    }
+                }
+                else if (id.StartsWith("CS", StringComparison.Ordinal))
+                {
+                    foreach (string codeFixId in CodeFixMap.GetCodeFixIds(id))
+                    {
+                        settings.Set(id, codeFixId, isEnabled);
+                    }
+                }
+                else
+                {
+                    Debug.Fail(id);
+                }
+            }
+#endif
+            return settings;
+        }
+#if VSCODE
+        private void Set(string compilerDiagnosticId, string codeFixId, bool isEnabled)
+        {
+            Set(new CodeFixIdentifier(compilerDiagnosticId, codeFixId), isEnabled);
+        }
+#endif
+        public bool IsEnabled(string compilerDiagnosticId, string codeFixId)
+        {
+            return IsEnabled(new CodeFixIdentifier(compilerDiagnosticId, codeFixId));
+        }
+    }
+}
--- src/Common/RefactoringSettings.cs
+++ src/Common/RefactoringSettings.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
+
 namespace Roslynator
 {
     public sealed class RefactoringSettings : CodeAnalysisSettings<string>
@@ -9,12 +11,14 @@ namespace Roslynator
         private static RefactoringSettings LoadSettings()
         {
             var settings = new RefactoringSettings();
-
-            //TODO: vscode
 #if VSCODE
-#endif
-            VisualStudioCode.CopyConfiguration(VisualStudioCode.Configuration, settings);
+            foreach (KeyValuePair<string, bool> kvp in VisualStudioCode.Configuration.Refactorings)
+            {
+                settings.Set(kvp.Key, kvp.Value);
+            }
 
+            settings.PrefixFieldIdentifierWithUnderscore = VisualStudioCode.Configuration.PrefixFieldIdentifierWithUnderscore;
+#endif
             return settings;
         }
 
